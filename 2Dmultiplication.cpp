7a5b49071b35213807fbc3026bbc747e7f423613
SartyGirho <46842786+SartyGirho@users.noreply.github.com>
2019-06-02 12:40:01 +0530

    Add files via upload
    C++ Code for 2-Dimensional Matrix Multiplication.

---
+++ 2Dmultiplication.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[3][3],b[3][3],c[3][3],i,j,mul,k;
+
+    cout<<"ENTER THE FIRST ARRAY OF NUMBERS "<<endl;
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            cin>>a[i][j];
+        }cout<<endl;
+    }
+    cout<<"ENTER THE SECOND ARRAY OF NUMBERS "<<endl;
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            cin>>b[i][j];
+        }cout<<endl;
+    }
+for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            mul=0;
+            for(k=0;k<=2;k++)
+                mul=mul+a[i][k]*b[k][j];
+            c[i][j]=mul;
+        }
+    }
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            cout<<c[i][j]<<("\t");
+        }cout<<endl;
+    }
+        return 0;
+    }
